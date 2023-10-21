b9342556f96b4cc5a6cf90529c7d73d44335974a
Rababmedle <rabab.elmamy@gmail.com>
2023-10-21 16:36:02 +0200

    TP-NG

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: ` <h1>Welcome to {{ title }}!</h1>`,
+  template: ` <h1>Welcome to {{ pokemons[0] }}!</h1>`,
 })
-export class AppComponent {
-  title = "app1";
+export class AppComponent implements OnInit {
+  pokemons = ["Bulbizare", "Salameche", "Carapuce"];
+  ngOnInit() {
+    console.table(this.pokemons);
+  }
 }
